a924ae437c14704a31a69f2aa91d4dc895bdeedf
raimkulovr <58400439+raimkulovr@users.noreply.github.com>
2020-11-16 14:25:57 +0600

    github-finder hw

--- day11 - github-finder/src/pages/Home.vue
+++ day11 - github-finder/src/pages/Home.vue
@@ -10,12 +10,21 @@
           <search :value="search" placeholder="type username" @search="search = $event" />
           
           <button v-if="!repos" class="btn btnPrimary" @click="getRepos">Search</button>
-          <button v-else class="btn btnPrimary" @click="getRepos">Search Again</button>
+          <button v-else class="btn btnPrimary" @click="getRepos" >Search Again</button>
           
-          
+          <div class="repos-head" v-for="profEl in prof" :key="profEl.index">
+              <img :src="profEl.data.avatar_url" alt="avatar">
+
+              <div class="repos-head-item">  
+                <span>login: {{profEl.data.login}}</span><br>
+                <a :href="profEl.data.url" class="link">go to profile</a>
+              </div>
+          </div>
 
           <div class="repos-wrapper" v-if="repos">
+            
             <div class="repo-item" v-for="repo in repos" :key="repo.id">
+              
 
               <div class="repos-info">
                 <a target="_blank" :href="repo.html_url" class="link">{{repo.name}}</a>
@@ -40,7 +49,8 @@ export default {
     return{
       search: '',
       error: null,
-      repos: null
+      repos: null,
+      prof: []
     }
   },
   methods: {
@@ -56,6 +66,23 @@ export default {
             this.repos=null
             this.error="Can't find this user"
           })
+      axios
+        .get(`https://api.github.com/users/${this.search}`)
+          .then(resPr => {
+            this.error=null
+            this.prof=[]
+            this.prof.push(resPr)
+            
+          })
+          .catch(err=> {
+            console.log(err)
+            this.prof=null
+            this.error="Can't find this user"
+          })
+      console.log(this.prof)
+    },
+    getProfile(){
+      console.log("yeah its profile button")
     }
   }
   
@@ -90,5 +117,25 @@ button{
 .error{
   margin-bottom: 20px;
   color: rgb(207, 52, 52);
+}
+img{
+    height:150px;
+    width: 150px;
+  }
+.repos-head{
+  margin-top: 40px;
+  width: 600px;
+  max-height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+}
+
+.repos-head-item{
+  margin-left: 40px;
+  
+    font-size: 30px;
+  
 }
 </style>
